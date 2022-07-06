cc0420531ad1b6a38f3ee1776faa2f47cb32a03f
Angela Caldas <aes.caldas@gmail.com>
2022-07-06 20:51:37 -0300

    :recycle: refactor Header code to handle user pic

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,31 @@
 // dependencies
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+// assets
+import userPic from '../assets/user.svg';
 
 const Header = () => {
+  const location = useLocation();
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    if (location.pathname === '/' || location.pathname === '/login' || location.pathname === '/cadastro') {
+      setUser('');
+    } else {
+      setUser(<img className='header__user' src={userPic} alt="Usuário" />);
+    }
+  }, [location]);
+
   return (
     <header className='header'>
       <nav>
-        <img className='header__logo' src="logo-clear.svg" alt="Logo AdoPet" />
-        <Link className='header__home' to="/" aria-label='Ir para Home'></Link>
-        <Link className='header__message' to="/" aria-label='Ir para Mensagens'></Link>
+        <div>
+          <img className='header__logo' src="logo-clear.svg" alt="Logo AdoPet" />
+          <Link className='header__home' to="/" aria-label='Ir para Home'></Link>
+          <Link className='header__message' to="/" aria-label='Ir para Mensagens'></Link>
+        </div>
+        {user}
       </nav>
     </header>
   );
