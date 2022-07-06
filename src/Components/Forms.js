06c983a2f0ba29b44081b1ad93c59114b0853c3b
Angela Caldas <aes.caldas@gmail.com>
2022-07-06 14:04:45 -0300

    :sparkles: Forms component

--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -4,45 +4,46 @@ import styles from './Forms.module.css';
 // dependencies
 import { useLocation } from 'react-router-dom';
 
+// components
+import Button from './Button.js';
+
 const Register = () => {
   const location = useLocation();
 
   return (
     <section className={styles.register}>
       <img src="logo-blue.svg" alt="" />
-      <p>Ainda não tem cadastro?</p>
-      <p>Então, antes de buscar seu melhor amigo, precisamos de alguns dados:</p>
-      {location.pathname === '/cadastro' ? (
-        <form>
-          <label>E-mail
-            <input type="email" />
-          </label>
-          <label>Nome
-            <input type="text" />
-          </label>
-          <label>Senha
-            <input type="password" />
-          </label>
-          <label>Confirme sua senha
-            <input type="password" />
-          </label>
-        </form>
-      ) : (
-        <form>
-          <label>
-            <input type="email" />
-          </label>
-          <label>
-            <input type="email" />
-          </label>
-          <label>
-            <input type="email" />
-          </label>
-          <label>
-            <input type="email" />
-          </label>
-        </form>)}
-    </section>
+      {
+        location.pathname === '/cadastro' ? (
+          <>
+            <p>Ainda não tem cadastro?</p>
+            <p>Então, antes de buscar seu melhor amigo, precisamos de alguns dados:</p>
+            <form>
+              <label htmlFor='email'>E-mail</label>
+              <input id='email' type="email" placeholder='Escolha seu melhor email' />
+              <label htmlFor='name'>Nome</label>
+              <input id='name' type="text" placeholder='Digite seu nome completo' />
+              <label htmlFor='pass-create'>Senha</label>
+              <input id='pass-create' type="password" placeholder='Crie uma senha' />
+              <label htmlFor='pass-confirm'>Confirme sua senha</label>
+              <input id='pass-confirm' type="password" placeholder='Crie uma senha' />
+              <Button children='Cadastrar' />
+            </form>
+          </>
+        ) : (
+          <>
+            <p style={{ height: 72, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>Já tem conta? Faça seu login:</p>
+            <form>
+              <label htmlFor='email'>E-mail</label>
+              <input id='email' type="email" placeholder='Insira seu email' />
+              <label htmlFor='pass'>Senha</label>
+              <input id='pass' type="password" placeholder='Insira sua senha' />
+              <Button children='Entrar' />
+            </form>
+          </>
+        )
+      }
+    </section >
   );
 };
 
