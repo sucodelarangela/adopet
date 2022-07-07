d11274242e02e21ed5ae67da10d1eed3f4ee6d54
Angela Caldas <aes.caldas@gmail.com>
2022-07-07 19:51:09 -0300

    :sparkles: Home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,20 +4,33 @@ import styles from './Home.module.css';
 // assets
 import { pets } from '../data/data.js';
 
+// dependencies
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
     <section className={styles.home}>
-      <p>Olá! Veja os amigos disponíveis para adoção!</p>
-      <div className="cards">
+      <div className={styles.cards}>
+        <p>Olá! Veja os amigos disponíveis para adoção!</p>
         {
           pets.map((pet, i) => (
             <>
-              <h3 key={i}>{pet.name}</h3>
+              <div key={i} className={styles.card}>
+                <img src={pet.img} alt={pet.name} />
+                <h4>{pet.name}</h4>
+                <ul>
+                  <li>{pet.age}</li>
+                  <li>{pet.size}</li>
+                  <li>{pet.behavior}</li>
+                </ul>
+                <p className={styles.card__city}>{pet.city}</p>
+                <Link className={styles.card__contact} to="/mensagem" aria-label='Falar com responsável'>Falar com responsável</Link>
+              </div>
             </>
           ))
         }
       </div>
-    </section>
+    </section >
   );
 };
 
