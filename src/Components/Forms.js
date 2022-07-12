727bcdf7d9a5f863a3dc497a8035a53359ac12c2
Angela Caldas <aes.caldas@gmail.com>
2022-07-12 20:20:32 -0300

    :recycle: refactor: new Input component for better handling forms

--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -1,26 +1,13 @@
-// dependencies
+// dependencies and hooks
 import { Link, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 // components
 import Button from './Button.js';
-import { useEffect } from 'react';
+import Input from './Input.js';
 
 const Register = () => {
   const location = useLocation();
-  let element = '';
-
-  const changeType = (id) => {
-    element = document.querySelector(id);
-
-    if (element.type === 'password') {
-      element.setAttribute('type', 'text');
-    } else {
-      element.setAttribute('type', 'password');
-    }
-  };
-
-  useEffect(() => {
-  }, [element]);
 
   return (
     <section className='register'>
@@ -30,40 +17,21 @@ const Register = () => {
           <>
             <p>Ainda não tem cadastro? <br /> Então, antes de buscar seu melhor amigo, precisamos de alguns dados:</p>
             <form>
-              <label htmlFor='email'>E-mail</label>
-              <input id='email' type="email" placeholder='Escolha seu melhor email' />
-              <label htmlFor='name'>Nome</label>
-              <input id='name' type="text" placeholder='Digite seu nome completo' />
-              <label htmlFor='pass-create'>Senha</label>
-              <span>
-                <input id='pass-create' type='password' placeholder='Crie uma senha' />
-                <span onClick={() => changeType('#pass-create')} className='pass__view'></span>
-              </span>
-              <label htmlFor='pass-confirm'>Confirme sua senha</label>
-              <span>
-                <input id='pass-confirm' type='password' placeholder='Repita a senha criada acima' />
-                <span onClick={() => changeType('#pass-confirm')} className='pass__view'></span>
-              </span>
-              <Link to='/login'>
-                <Button children='Cadastrar' />
-              </Link>
+              <Input id='email' type='email' placeholder='Escolha seu melhor email' children='E-mail' />
+              <Input id='name' type='text' placeholder='Digite seu nome completo' children='Nome' />
+              <Input id='pass-create' type='password' placeholder='Crie uma senha' children='Senha' />
+              <Input id='pass-confirm' type='password' placeholder='Repita a senha criada acima' children='Confirme sua senha' />
+              <Link className='button' to='/login'>Cadastrar</Link>
             </form>
           </>
         ) : (
           <>
-            <p style={{ height: 72, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>Já tem conta? Faça seu login:</p>
+            <p>Já tem conta? Faça seu login:</p>
             <form>
-              <label htmlFor='email'>E-mail</label>
-              <input id='email' type="email" placeholder='Insira seu email' />
-              <label htmlFor='pass'>Senha</label>
-              <span>
-                <input id='pass' type='password' placeholder='Insira sua senha' />
-                <span onClick={() => changeType('#pass')} className='pass__view'></span>
-              </span>
-              <a href="#">Esqueci minha senha</a>
-              <Link to='/home'>
-                <Button children='Entrar' />
-              </Link>
+              <Input id='email' type='email' placeholder='Insira seu email' children='E-mail' />
+              <Input id='pass' type='password' placeholder='Insira sua senha' children='Senha' />
+              <a class='register__forgot' href="#">Esqueci minha senha</a>
+              <Link className='button' to='/home'>Entrar</Link>
             </form>
           </>
         )
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,6 +1,30 @@
-const Input = () => {
+import { useState } from 'react';
+
+const Input = ({ id, type, placeholder, children }) => {
+  const [inputType, setInputType] = useState(type);
+
+  const changeType = () => {
+    if (inputType === 'password') {
+      setInputType('text');
+    } else {
+      setInputType('password');
+    }
+  };
+
   return (
-    <div>Input</div>
+    <>
+      <label htmlFor={id}>{children}</label>
+      {
+        type === 'password' ? (
+          <span>
+            <span onClick={() => changeType(type)} className='pass__view'></span>
+            <input id={id} name={id} type={inputType} placeholder={placeholder} />
+          </span>
+        ) : (
+          <input id={id} name={id} type={type} placeholder={placeholder} />
+        )
+      }
+    </>
   );
 };
 
